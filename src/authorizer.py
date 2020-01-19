8f145b3702a133bfad9637bb0327ac8581b4a3ea
santech202 <santech202@gmail.com>
2020-01-19 17:37:42 +0100

    Create lambda_handler
    Create lambda_handler and test it

---
+++ src/authorizer.py
@@ -0,0 +1,70 @@
+import json
+
+import jwt
+import logging
+from jwt.algorithms import RSAAlgorithm
+
+from src.jwks import get_jwks
+from src.model.auth_policy_response import AuthPolicyResponse
+from src.model.effect_verb import Effect
+from src.model.http_verb import HttpVerb
+from src.model.policy_document import PolicyDocument
+from src.model.statement import Statement
+from src.util.method_arn_util import MethodArnUtil
+from src.environment_parameters import EnvironmentParameters
+from src.util.resource_arn_util import ResourceArnUtil
+
+
+def lambda_handler(event, context):
+    environment_parameters = EnvironmentParameters()
+    token = event['authorizationToken'].split(' ')[1]
+    method_arn_str = event['methodArn']
+
+    claims = jwt.decode(token, verify=False)
+
+    try:
+        _assert_issuer_match_user_pool(environment_parameters.issuer_url, claims)
+        _assert_signature_is_valid(token, environment_parameters)
+
+    except Exception as e:
+        logging.info(e)
+        raise Exception('Unauthorized')
+
+    method_arn_util = MethodArnUtil(method_arn_str)
+    auth_policy_response = _build_auth_policy(claims, method_arn_util)
+    return auth_policy_response.build_response(is_json=True)
+
+
+def _assert_issuer_match_user_pool(issuer_url, claims):
+    iss = claims['iss']
+    if issuer_url != iss:
+        raise Exception('Token {} not match user pool {}'.format(iss, issuer_url))
+
+
+def _assert_signature_is_valid(token, environment_parameters):
+    public_keys = get_jwks(environment_parameters.issuer_url)
+    headers = jwt.get_unverified_header(token)
+
+    kid = headers['kid']
+    if kid not in public_keys:
+        raise Exception('No public kid {} in JWK set    '.format(kid))
+
+    public_key = RSAAlgorithm.from_jwk(json.dumps(public_keys[kid]))
+    jwt.decode(token, public_key, algorithms='RS256', audience=environment_parameters.client_ids,
+               issuer=environment_parameters.issuer_url)
+
+
+def _build_auth_policy(claims, method_arn_util):
+    principal_id = claims["sub"]
+    policy_document = PolicyDocument()
+    auth_policy_response = AuthPolicyResponse(principal_id, policy_document)
+
+    resource_arn_util = ResourceArnUtil(method_arn_util.region, method_arn_util.aws_account_id,
+                                        method_arn_util.rest_api_id, method_arn_util.stage)
+
+    resource_arn = resource_arn_util.generate_resource_arn(HttpVerb.ALL, "*")
+    statement = Statement(Effect.ALLOW, resource_arn)
+
+    policy_document.add_statement(statement)
+
+    return auth_policy_response
---
+++ test/test_authorizer.py
@@ -0,0 +1,146 @@
+import json
+import os
+import jwt
+import unittest
+import uuid
+from unittest.mock import patch
+from Crypto.PublicKey import RSA
+from datetime import datetime, timedelta
+
+from jwt.utils import force_unicode, to_base64url_uint
+
+from src.authorizer import lambda_handler
+
+
+def build_event_set(token):
+    return {
+        "type": "TOKEN",
+        "methodArn": "arn:aws:execute-api:eu-west-1:111111111111:qwert12345/dev/GET/v1/jobs",
+        "authorizationToken": "Bearer " + token
+    }
+
+
+def build_response_jwks_set(kid, modulus, exponent):
+    return {
+        kid:
+            {
+                "alg": "RS256", "e": exponent, "kid": kid, "kty": "RSA",
+                "use": "sig",
+                "n": modulus
+            }
+    }
+
+
+def build_claims(sub, case_study):
+    after_one_hours_from_now = datetime.now() + timedelta(hours=1)
+    before_one_hours_from_now = datetime.now() - timedelta(hours=1)
+    return {
+        "at_hash": "123123",
+        "sub": sub,
+        "cognito:groups": [
+            "admin"
+        ],
+        "iss": "https://test.com" if case_study == "different_issuer" else "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_abcde1234",
+        "cognito:username": "test@klarna.com",
+        "nonce": "n-0S6_WzA2Mj",
+        "aud": "123123" if case_study == "different_audience" else "qwertuiop123654789",
+        "token_use": "id",
+        "auth_time": before_one_hours_from_now.timestamp(),
+        "exp": before_one_hours_from_now.timestamp() if case_study == "token_expired" else after_one_hours_from_now.timestamp(),
+        "iat": before_one_hours_from_now.timestamp(),
+        "email": "test@test.com"
+    }
+
+
+class TestAuthorizer(unittest.TestCase):
+    kid = "11233112233="
+    private_key = ""
+    private_key_2 = ""
+    exponent = ""
+    modulus = ""
+    sub = ""
+
+    def setUp(self):
+        key = RSA.generate(2048)
+        key_2 = RSA.generate(2048)
+        self.private_key = key.exportKey().decode("utf-8")
+        self.private_key_2 = key_2.exportKey().decode("utf-8")
+        self.exponent = force_unicode(to_base64url_uint(key.e))
+        self.modulus = force_unicode(to_base64url_uint(key.n))
+        self.sub = str(uuid.uuid1())
+        os.environ["USER_POOL_ID"] = "eu-west-1_abcde1234"
+        os.environ["CLIENT_IDS"] = "qwertuiop123654789"
+        os.environ["REGION"] = "eu-west-1"
+
+    @patch("src.authorizer.get_jwks")
+    def test_lambda_handler__should_return_policy_with_accept__when_everything_is_ok(self, mock_get_jwks):
+        mock_get_jwks.return_value = build_response_jwks_set(self.kid, self.modulus, self.exponent)
+        token = jwt.encode(build_claims(self.sub, ""), self.private_key, algorithm='RS256', headers={"kid": self.kid})
+        event = build_event_set(token.decode("utf-8"))
+
+        response = lambda_handler(event, None)
+        json_data = json.loads(response)
+        self.assertEqual(self.sub, json_data["principalId"])
+        self.assertEqual(0, len(json_data["context"]))
+        self.assertEqual("2012-10-17", json_data["policyDocument"]["Version"])
+        self.assertEqual(1, len(json_data["policyDocument"]["Statement"]))
+        self.assertEqual("Allow", json_data["policyDocument"]["Statement"][0]["Effect"])
+        self.assertEqual("execute-api:Invoke", json_data["policyDocument"]["Statement"][0]["Action"])
+        self.assertEqual("arn:aws:execute-api:eu-west-1:111111111111:qwert12345/dev/*/*",
+                         json_data["policyDocument"]["Statement"][0]["Resource"])
+
+    @patch("src.authorizer.get_jwks")
+    def test_lambda_handler__should_return_unauthorized__when_token_expired(self, mock_get_jwks):
+        mock_get_jwks.return_value = build_response_jwks_set(self.kid, self.modulus, self.exponent)
+        token = jwt.encode(build_claims(self.sub, "token_expired"), self.private_key, algorithm='RS256',
+                           headers={"kid": self.kid})
+        event = build_event_set(token.decode("utf-8"))
+
+        with self.assertRaises(Exception) as context:
+            lambda_handler(event, None)
+            self.fail("Expect Exception")
+
+        self.assertEqual(context.exception.args[0], 'Unauthorized')
+
+    @patch("src.authorizer.get_jwks")
+    def test_lambda_handler__should_return_unauthorized__when_token_has_different_audience(self, mock_get_jwks):
+        mock_get_jwks.return_value = build_response_jwks_set(self.kid, self.modulus, self.exponent)
+        token = jwt.encode(build_claims(self.sub, "different_audience"), self.private_key, algorithm='RS256',
+                           headers={"kid": self.kid})
+        event = build_event_set(token.decode("utf-8"))
+
+        with self.assertRaises(Exception) as context:
+            lambda_handler(event, None)
+            self.fail("Expect Exception")
+
+        self.assertEqual(context.exception.args[0], 'Unauthorized')
+
+    @patch("src.authorizer.get_jwks")
+    def test_lambda_handler__should_return_unauthorized__when_token_has_different_issuer(self, mock_get_jwks):
+        mock_get_jwks.return_value = build_response_jwks_set(self.kid, self.modulus, self.exponent)
+        token = jwt.encode(build_claims(self.sub, "different_issuer"), self.private_key, algorithm='RS256',
+                           headers={"kid": self.kid})
+        event = build_event_set(token.decode("utf-8"))
+
+        with self.assertRaises(Exception) as context:
+            lambda_handler(event, None)
+            self.fail("Expect Exception")
+
+        self.assertEqual(context.exception.args[0], 'Unauthorized')
+
+    @patch("src.authorizer.get_jwks")
+    def test_lambda_handler__should_return_unauthorized__when_rsa_not_much(self, mock_get_jwks):
+        mock_get_jwks.return_value = build_response_jwks_set(self.kid, self.modulus, self.exponent)
+        token = jwt.encode(build_claims(self.sub, ""), self.private_key_2, algorithm='RS256',
+                           headers={"kid": self.kid})
+        event = build_event_set(token.decode("utf-8"))
+
+        with self.assertRaises(Exception) as context:
+            lambda_handler(event, None)
+            self.fail("Expect Exception")
+
+        self.assertEqual(context.exception.args[0], 'Unauthorized')
+
+
+if __name__ == '__main__':
+    unittest.main()
