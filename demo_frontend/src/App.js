c7a872e0cee038f00ec8d00dac204ce7eb5914a8
santech202 <santech202@gmail.com>
2020-02-16 10:47:34 +0100

    Update Demo

--- demo_frontend/src/App.js
+++ demo_frontend/src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator } from "aws-amplify-react";
 
+const regionId = "{regionId}"
+const userPoolId = "{userPoolId}"
+const clientId = "{clientId}"
+const cognitoDomain = "{cognitoDomain}.auth." + regionId + ".amazoncognito.com"
+const apiGateway = "https://{apiId}.execute-api." + regionId + ".amazonaws.com/{stage}"
+
 Amplify.configure({
 	Auth: {
-		"region": '{regionId}',
-		"userPoolId": '{userpoolID}',
-		"userPoolWebClientId": 'CLIENT_ID',
+		"region": regionId,
+		"userPoolId": userPoolId,
+		"userPoolWebClientId": clientId,
 		"mandatorySignIn": true,
 		"oauth": {
-			"domain": "{cognitoDomain}.auth.{regionId}.amazoncognito.com",
+			"domain": cognitoDomain,
 			"scope": ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
 			"redirectSignIn": "http://localhost:3000/",
 			"redirectSignOut": "http://localhost:3000/",
@@ -20,13 +26,12 @@ Amplify.configure({
 	}
 });
 
-
 class App extends Component {
 	componentDidMount() {
 		Auth.currentSession()
 			.then((res) => {
 				const jwtToken = res.getIdToken().getJwtToken();
-				fetch('https://{apiId}.execute-api.{regionId}.amazonaws.com/{stage}/demo', {
+				fetch(apiGateway + '/demo', {
 						headers: {
 							'Authorization': 'Bearer ' + jwtToken
 						}
